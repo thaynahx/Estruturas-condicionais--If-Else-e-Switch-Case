cf99c051b086c0351590ec3137bc2122242062b2
thaynahx <80104674+thaynahx@users.noreply.github.com>
2024-04-29 17:42:45 -0300

    Update and rename Exer_1.java to Exer01.java

--- Exer01.java
+++ Exer01.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Exer_1 {
 	public static void main(String[] args) {
 		Scanner ler =new Scanner(System.in);
-		System.out.println("*****Número maior*****");
+		System.out.println("*****NÃºmero maior*****");
 		
-		System.out.println("Entre com o primeiro número:");
+		System.out.println("Entre com o primeiro nÃºmero:");
 		int numero1 =ler.nextInt();
-		System.out.println("Entre com o segundo número:");
+		System.out.println("Entre com o segundo nÃºmero:");
 		int numero2 =ler.nextInt();
 		
 		if (numero1 > numero2) {
-			System.out.println("Número 1 é maior. ");
+			System.out.println("NÃºmero 1 Ã© maior. ");
 		}else {
-			System.out.println("Número 2 é maior.");
+			System.out.println("NÃºmero 2 Ã© maior.");
 		}
 	}
 	
