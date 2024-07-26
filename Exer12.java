229251f0c823a3b33edf085862cdb72a0c72e9d1
thaynahx <80104674+thaynahx@users.noreply.github.com>
2024-07-26 19:55:28 -0300

    Update Exer12.java

--- Exer12.java
+++ Exer12.java
@@ -1,3 +1,25 @@
+/*12.Faça um programa para o cálculo de uma folha de pagamento,
+sabendo que os descontos são do Imposto de Renda, que depende do
+salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o
+FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é
+a empresa que deposita). O Salário Líquido corresponde ao Salário
+Bruto menos os descontos. O programa deverá pedir ao usuário o
+valor da sua hora e a quantidade de horas trabalhadas no mês.
+o Desconto do IR:
+o Salário Bruto até 900 (inclusive) - isento
+o Salário Bruto até 1500 (inclusive) - desconto de 5%
+o Salário Bruto até 2500 (inclusive) - desconto de 10%
+o Salário Bruto acima de 2500 - desconto de 20% Imprima na tela
+as informações, dispostas conforme o exemplo abaixo. No
+exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+Salário Bruto: (5 * 220) : R$ 1100,00
+(-) IR (5%) : R$ 55,00
+(-) INSS ( 10%) : R$ 110,00
+FGTS (11%) : R$ 121,00
+Total de descontos : R$ 165,00
+Salário Liquido : R$ 935,00*/
+
 package Exercicio_15;
 
 import java.util.Scanner;
@@ -7,7 +29,7 @@ public class Exer12 {
 	public static void main(String[] args) {
 
 	Scanner ler =new Scanner(System.in);
-	System.out.println("*****C�uculo para Folha de pagamento: *****\n");
+	System.out.println("*****Cáuculo para Folha de pagamento: *****\n");
 	
 	System.out.println("Entre com o valor da sua hora: (Valor/hora)");
 	double valorHora =ler.nextDouble();
@@ -42,7 +64,7 @@ public class Exer12 {
 	System.out.println("|(-) INSS ( 10%)                              : R$ "+ inss+" |");
 	System.out.println("|FGTS (11%)                                   : R$ "+ fgts+" |");
 	System.out.println("|Total de descontos                           : R$ " + descontos+" |");
-	System.out.println("|Salario L�quido                              : R$ " + salarioLiquido+" |");
+	System.out.println("|Salario Líquido                              : R$ " + salarioLiquido+" |");
 	System.out.println("|________________________________________________________|");
 	
 	
