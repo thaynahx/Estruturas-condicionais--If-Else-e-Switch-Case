59dc420645b3546ca0a3d8c6dcdaf0b629553c2d
thaynahx <80104674+thaynahx@users.noreply.github.com>
2024-07-26 20:18:07 -0300

    Update Exer16.java

--- Exer16.java
+++ Exer16.java
@@ -1,3 +1,18 @@
+/*16.Faça um programa que calcule as raízes de uma equação do segundo
+grau, na forma ax2 + bx + c. O programa deverá pedir os valores de a,
+b e c e fazer as consistências, informando ao usuário nas seguintes
+situações:
+
+a. Se o usuário informar o valor de A igual a zero, a equação não é
+do segundo grau e o programa não deve fazer pedir os demais
+valores, sendo encerrado;
+b. Se o delta calculado for negativo, a equação não possui raizes
+reais. Informe ao usuário e encerre o programa;
+c. Se o delta calculado for igual a zero a equação possui apenas
+uma raiz real; informe-a ao usuário;
+d. Se o delta for positivo, a equação possui duas raiz reais;
+informe-as ao usuário;*/
+
 package Exercicio_15;
 
 import java.util.Scanner;
@@ -11,7 +26,7 @@ public class Exer16 {
         System.out.println("Entre com o valor de a:");
         int a = scan.nextInt();
         if (a == 0){
-            System.out.println("N�o � equa��o de segundo grau");
+            System.out.println("Não é equação de segundo grau");
         } else {
 
             System.out.println("Entre com o valor de b:");
