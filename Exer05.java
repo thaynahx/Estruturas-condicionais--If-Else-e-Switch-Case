ef8b3f806cf374f5399eb8d75e351921a69aca75
thaynahx <80104674+thaynahx@users.noreply.github.com>
2024-07-26 19:42:31 -0300

    Update Exer05.java

--- Exer05.java
+++ Exer05.java
@@ -1,3 +1,11 @@
+/*5- Faça um programa para a leitura de duas notas parciais de um aluno.
+O programa deve calcular a média alcançada por aluno e apresentar:
+o A mensagem "Aprovado", se a média alcançada for maior ou
+igual a sete;
+o A mensagem "Reprovado", se a média for menor do que sete;
+o A mensagem "Aprovado com Distinção", se a média for igual a
+dez. */
+
 package Exercicio_15;
 
 import java.util.Scanner;
