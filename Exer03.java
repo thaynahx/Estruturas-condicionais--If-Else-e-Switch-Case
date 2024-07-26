75d738bde1808796ce5201dd69691e93e3134506
thaynahx <80104674+thaynahx@users.noreply.github.com>
2024-07-26 19:39:34 -0300

    Update Exer03.java

--- Exer03.java
+++ Exer03.java
@@ -1,3 +1,7 @@
+/*3- Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+*/ 
+
 package Exercicio_15;
 
 import java.util.Scanner;
