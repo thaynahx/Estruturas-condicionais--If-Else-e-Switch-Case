3a5402a381f12c9168407e79c8fe0033d4694983
Thaynah <80104674+thaynahx@users.noreply.github.com>
2024-07-27 21:24:43 -0300

    Update Exer13.java

--- Exer13.java
+++ Exer13.java
@@ -27,6 +27,27 @@ public class Exer13 {
 			break;
 		}
 
+		/*
+		int diaSemana = ler.nextInt();
+		if (diaSemana == 1 ) {
+			System.out.println("Domingo");
+		} else if (diaSemana == 2 ) {
+			System.out.println("Segunda=feira");
+		}else if (diaSemana == 3 ) {
+			System.out.println("Terça-feira");
+		}else if (diaSemana == 4 ) {
+			System.out.println("Quarta=feira");
+		}else if (diaSemana == 5 ) {
+			System.out.println("Quinta-feira");
+		}else if (diaSemana == 6 ) {
+			System.out.println("Sexta-feira");
+		}else if (diaSemana == 7 ) {
+			System.out.println("Sábado");
+		} else {
+			System.out.println("Valor inválido, tente novamente!");
+		}
+		*/
+
 		
 		
 		
